c20fcb922bb08b0df2c67cf93fd646ac394df57d
l <l@l.com>
2015-09-14 14:47:56 -0700

    Initial Commit

---
+++ imaginaryAnimalsLister/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  imaginaryAnimalsLister
+//
+//  Created by Leo Yu on 9/14/15.
+//  Copyright © 2015 Yu, Leo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
