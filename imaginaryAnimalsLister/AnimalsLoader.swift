0a884e2b384f22f5cd142c2600b37eb90af9668f
David Casini <dcasini9@icloud.com>
2015-09-15 10:11:39 -0700

    changed some heights

--- imaginaryAnimalsLister/AnimalsLoader.swift
+++ imaginaryAnimalsLister/AnimalsLoader.swift
@@ -15,10 +15,10 @@ struct AnimalsLoader
         let mermaid = ImaginaryAnimal(name: "Mermaid", height: 1.5,  location: "Oceans", dateLastSeen: "1858",
             imageURL: NSURL(string: "https://upload.wikimedia.org/wikipedia/commons/2/2a/Waterhouse_a_mermaid.jpg"));
         
-        let lochness = ImaginaryAnimal(name: "Loch Ness Monster", height: 1.5,  location: "Scotland", dateLastSeen: "1951",
+        let lochness = ImaginaryAnimal(name: "Loch Ness Monster", height: 85,  location: "Scotland", dateLastSeen: "1951",
             imageURL: NSURL(string: "https://s3.amazonaws.com/glowscript/textures/flower_texture.jpg"));
         
-        let bigfoot = ImaginaryAnimal(name: "Bigfoot", height: 1.5,  location: "North America", dateLastSeen: "1963",
+        let bigfoot = ImaginaryAnimal(name: "Bigfoot", height: 5.8,  location: "North America", dateLastSeen: "1963",
             imageURL: NSURL(string: "https://s3.amazonaws.com/glowscript/textures/earth_texture.jpg"));
         
         let ogopogo = ImaginaryAnimal(name: "Ogopogo", height: 3.2,  location: "Oceans", dateLastSeen: "1987",
